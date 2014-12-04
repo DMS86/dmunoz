6b6c5f7c601df4e18f01aed51a459c5bdbe6372d
DMS86 <dmunozs86@gmail.com>
2014-12-04 18:26:45 -0300

    bugs fixed, agregados features

--- website/static/js/src/viewModel.js
+++ website/static/js/src/viewModel.js
@@ -6,8 +6,14 @@ function viewModel() {
     self.notaMaxima = ko.observable(7);
     self.totalPuntos = ko.observable(10);
     self.porcentaje = ko.observable(60);
-    self.redondear4ParaAbajo = ko.observable(false);
+    self.redondearNotaLimiteParaAbajo = ko.observable(false);
     self.notaLimite = ko.observable(4);
+
+    self.checkValoresNotas = function() {
+        if (self.notaMaxima() <= self.notaMinima()) {
+            alert("La nota máxima debe ser mayor que la nota mínima. Los datos de la tabla no serán correctos.");
+        }
+    }
 
     self.puntajeNotaLimite = ko.computed(
         function() {
@@ -15,7 +21,7 @@ function viewModel() {
             puntaje *= 2;
             var porcentaje = parseFloat(this.porcentaje())/100;
 
-            if (this.redondear4ParaAbajo()) {
+            if (this.redondearNotaLimiteParaAbajo()) {
                 puntaje = Math.floor(puntaje * porcentaje);
             } else {
                 puntaje = Math.round(puntaje * porcentaje);
@@ -25,14 +31,23 @@ function viewModel() {
     );
 
     self.sumaEnRojos = ko.computed(
-        function() { 
-            return (4 - parseFloat(self.notaMinima())) / (self.puntajeNotaLimite()); 
+        function() {
+            var notaLimite = parseFloat(self.notaLimite()); 
+            var notaMinima = parseFloat(self.notaMinima());
+            var puntajeNotaLimite = parseFloat(self.puntajeNotaLimite());
+            var resultado = (notaLimite - notaMinima) / puntajeNotaLimite; 
+            return (resultado == Infinity) ? 0 : resultado;
         }, self
     );
 
     self.sumaEnAzules = ko.computed(
         function() { 
-            return (7 - 4) / (parseInt(this.totalPuntos()) - (this.puntajeNotaLimite())); 
+            var notaMaxima = parseFloat(this.notaMaxima());
+            var notaLimite = parseFloat(this.notaLimite());
+            var totalPuntos = parseFloat(this.totalPuntos());
+            var puntajeNotaLimite = parseFloat(this.puntajeNotaLimite())
+            var resultado = (notaMaxima - notaLimite) / (totalPuntos - puntajeNotaLimite)
+            return (resultado == Infinity) ? 0 : resultado; 
         }, self
     );
 
@@ -42,6 +57,7 @@ function viewModel() {
 
     self.calcularNota = function (indice) {
         var notaMinima = parseFloat(self.notaMinima());
+        var notaLimite = parseFloat(self.notaLimite());
         var puntaje = indice / 2;
         var nota;
         var rojo = false;
@@ -49,7 +65,7 @@ function viewModel() {
             nota = notaMinima;
             rojo = true;
         } else {
-            nota = 4;
+            nota = notaLimite;
         }
         if (rojo) {
             nota = nota + (puntaje * self.sumaEnRojos());
