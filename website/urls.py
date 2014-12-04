730f371d1482db22635394ab41c13d61d5d82215
DMS86 <dmunozs86@gmail.com>
2014-12-04 15:17:05 -0300

    output for example index and escala de notas

--- website/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
-
-
-urlpatterns = patterns("",
-    url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
-    url(r"^tati/escala-de-notas/$", TemplateView.as_view(template_name="escala.html"), name="escala"),
-)
