ec1788f7ca0b9bdcc4b821195b535cb759b6da09
DMS86 <dmunozs86@gmail.com>
2014-12-05 12:30:04 -0300

    tabla de notas striped

--- fabfile.py
+++ fabfile.py
@@ -11,3 +11,26 @@ def restart(branch=None):
     if branch is not None:
         local('python manage.py loaddata ' + branch)
     local('python manage.py runserver')
+
+def gen():
+    local('python manage.py staticsitegen')
+    local('python manage.py collectstatic --noinput')
+
+def update_files():
+    local('git checkout gh-pages')
+    local('rm *.html')
+    local('rm -r escala-de-notas')
+    local('rm -r site_media')
+    local('git pull origin master -- _output')
+    local('mv _output/* .')
+    local('rm -r _output')
+    local('git pull origin master -- website/site_media')
+    local('mv website/site_media .')
+    local('rm -r website')
+    local('git commit -m 'website update'')
+    local('git push origin gh-pages')
+    local('git checkout master')
+
+def deploy():
+    gen()
+    update_files()
